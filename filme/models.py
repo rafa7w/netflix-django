229c12e9680008be25326c48254dd6d8391bf0a9
Rafael William <rafael.fim@hotmail.com>
2022-03-24 16:35:28 -0300

    movie model

--- filme/models.py
+++ filme/models.py
@@ -1,5 +1,19 @@
 # Arquivo onde serão criadas as tabelas do banco de dados
-
 from django.db import models
+from django.utils import timezone
+
+LISTA_CATEGORIAS = (
+  ('ANALISES', 'Análises'),
+  ('PROGRAMACAO', 'Programação'),
+  ('APRESENTACAO', 'Apresentação'),
+  ('OUTROS', 'Outros'),
+)
 
 # Create your models here.
+class Filme(models.Model):
+  titulo = models.CharField(max_length=100)
+  thumb = models.ImageField(upload_to='thumb_filmes')
+  descricao = models.TextField(max_length=1000)
+  categoria = models.CharField(max_length=15, choices=LISTA_CATEGORIAS)
+  visualizacoes = models.IntegerField(default=0)
+  data_criacao = models.DateTimeField(default=timezone.now)
