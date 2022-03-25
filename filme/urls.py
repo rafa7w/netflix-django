ac87dd8ee93e2835f2a561fdf022acd2e05f6c11
Rafael William <rafael.fim@hotmail.com>
2022-03-25 14:37:41 -0300

    passing parameters to html pages - context

--- filme/urls.py
+++ filme/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from .views import homepage
+from .views import homepage, homefilmes
 
 urlpatterns = [
     path('', homepage),
+    path('filmes/', homefilmes),
 ]
--- filme/views.py
+++ filme/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from .models import Filme
 
 # Create your views here.
 def homepage(req):
   return render(req, 'homepage.html')
+
+def homefilmes(req):
+  context = {}
+  lista_filmes = Filme.objects.all()
+  context['lista_filmes'] = lista_filmes
+  return render(req, 'homefilmes.html', context)
