4f808a2566393b2c84c7accc53f37e075b36ce44
Rafael William <rafael.fim@hotmail.com>
2022-04-03 15:35:35 -0300

    featured movie

--- filme/context.py
+++ filme/context.py
@@ -2,9 +2,9 @@ from .models import Filme
 
 def lista_filmes_recentes(req):
   lista_filmes = Filme.objects.all().order_by('-data_criacao')[:8]
-  return {'lista_filmes_recentes': lista_filmes}
+  filme_destaque = lista_filmes[0]
+  return {'lista_filmes_recentes': lista_filmes, 'filme_destaque': filme_destaque}
 
 def lista_filmes_emalta(req):
   lista_filmes = Filme.objects.all().order_by('-visualizacoes')[:8]
   return {'lista_filmes_emalta': lista_filmes}
-
